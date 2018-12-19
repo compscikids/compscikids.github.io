4be87513e58457f733a8f917f3ad5e52ef88ab3b
compscikids <43106803+compscikids@users.noreply.github.com>
2018-12-18 19:26:16 -0600

    fix slide show

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,49 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+var slideIndex = 0;
+var time;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides2(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides2(slideIndex = n);
+}
+
+function showSlides2(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    clearTimeout(time);
+    time = setTimeout(showSlides, 5000);
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if ((slideIndex) > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    time = setTimeout(showSlides, 8000); // Change image every 2 seconds
+}
 
 (function($) {
 
